d7286a6a9e225a64c199606b67ca946cc54b4f1c
marcosetm <m2e5090@gmail.com>
2025-07-08 20:34:33 -0700

    create account repository

--- src/main/java/com/marcosetm/user_management/repository/AccountRepository.java
+++ src/main/java/com/marcosetm/user_management/repository/AccountRepository.java
@@ -1,7 +1,12 @@
 package com.marcosetm.user_management.repository;
 
+import com.marcosetm.user_management.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AccountRepository extends JpaRepository {
+import java.util.Optional;
 
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
 }
