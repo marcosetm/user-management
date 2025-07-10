feb08c03c6598c69542e0a0581398d0d9f188614
marcosetm <m2e5090@gmail.com>
2025-07-09 17:16:21 -0700

    create AccountService and add pass encoder config

---
+++ src/main/java/com/marcosetm/user_management/service/AccountService.java
@@ -0,0 +1,24 @@
+package com.marcosetm.user_management.service;
+
+import com.marcosetm.user_management.model.Account;
+import com.marcosetm.user_management.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
+        this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Account createAccount(Account accountCreateRequest) {
+        accountCreateRequest.setPassword(passwordEncoder.encode(accountCreateRequest.getPassword()));
+        return accountRepository.save(accountCreateRequest);
+    }
+}
