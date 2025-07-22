7713845247cb84b0ad8853ac788021b19ce07117
marcosetm <m2e5090@gmail.com>
2025-07-21 21:18:43 -0700

    add cors config mapping

---
+++ src/main/java/com/marcosetm/user_management/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.marcosetm.user_management.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
